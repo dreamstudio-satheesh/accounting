e3dc133b4741834fb67bb7d08347a738a8a39b45
satheesh kumar <4798470+dreamstudio-satheesh@users.noreply.github.com>
2023-08-15 20:15:00 +0530

    add git ignore

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,14 +15,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        if (\Request::route()->getName() != 'LaravelUpdater::database') {
+         /* if (\Request::route()->getName() != 'LaravelUpdater::database') { */
             $this->call(UsersTableSeeder::class);
             $this->call(NotificationSeeder::class);
             $this->call(AiTemplateSeeder::class);
             Artisan::call('module:migrate LandingPage');
             Artisan::call('module:seed LandingPage');
-        } else {
+       /*  } else {
             Utility::languagecreate();
-        }
+        } */
     }
 }
